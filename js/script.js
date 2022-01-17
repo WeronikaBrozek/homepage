fa623ce35a7e4613a30b17dba179293621af20ff
Weronika Brożek <brozekweronika@gmail.com>
2022-01-17 17:07:34 +0100

    code refactoring

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
 {
     const welcome = () => {
         console.log("Witaj!");
-    }
-    
+    };
+
     const onChangeBackgroundClick = () => {
+        const body = document.querySelector(".js-body");
+        const section = document.querySelector(".js-section");
+        const link = document.querySelector(".js-link");
+        const image = document.querySelector(".js-image");
+        const themeName = document.querySelector(".js-themeName")
         body.classList.toggle("body--dark");
         section.classList.toggle("section--dark");
         image.classList.toggle("image--dark");
         link.classList.toggle("navBar__itemLink--dark");
-    
+
         if (body.classList.contains("body--dark")) {
             themeName.innerText = "jasny"
         } else {
             themeName.innerText = "ciemny"
         }
-    
-    }
-    
-    welcome();
-    
-    let button = document.querySelector(".js-button");
-    let body = document.querySelector(".js-body");
-    let link = document.querySelector(".js-link");
-    let section = document.querySelector(".js-section");
-    let image = document.querySelector(".js-image");
-    let themeName = document.querySelector(".js-themeName");
-    
-    button.addEventListener("click", onChangeBackgroundClick);
-    
-    
+
+    };
+
+    const init = () => {
+        let button = document.querySelector(".js-button");
+
+        button.addEventListener("click", onChangeBackgroundClick);
+
+        welcome();
+    };
+
+    init();
 }
