9b83f195d45abb5e3d332c90ee8a10438cbd6927
Weronika Brożek <brozekweronika@gmail.com>
2022-01-17 16:46:10 +0100

    button class name change

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ let body = document.querySelector(".js-body");
 let link = document.querySelector(".js-link");
 let section = document.querySelector(".js-section");
 let image = document.querySelector(".js-image")
-let themeName = document.querySelector(".js-button");
+let themeName = document.querySelector(".js-themeName");
 
 button.addEventListener("click", () => {
     body.classList.toggle("body--dark")
