afe8c138277a9947a273ab1b5cc753f0144df71d
Weronika Brożek <brozekweronika@gmail.com>
2022-01-17 16:57:25 +0100

    code refactoring

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
-function welcome() {
-    console.log("Witaj!");
-}
-
-welcome();
-
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let link = document.querySelector(".js-link");
-let section = document.querySelector(".js-section");
-let image = document.querySelector(".js-image")
-let themeName = document.querySelector(".js-themeName");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("body--dark")
-    section.classList.toggle("section--dark")
-    image.classList.toggle("image--dark")
-    link.classList.toggle("navBar__itemLink--dark");
-
-    if (body.classList.contains("body--dark")) {
-        themeName.innerText = "jasny"
-    } else {
-        themeName.innerText = "ciemny"
+{
+    const welcome = () => {
+        console.log("Witaj!");
     }
-
-});
-
+    
+    const onChangeBackgroundClick = () => {
+        body.classList.toggle("body--dark");
+        section.classList.toggle("section--dark");
+        image.classList.toggle("image--dark");
+        link.classList.toggle("navBar__itemLink--dark");
+    
+        if (body.classList.contains("body--dark")) {
+            themeName.innerText = "jasny"
+        } else {
+            themeName.innerText = "ciemny"
+        }
+    
+    }
+    
+    welcome();
+    
+    let button = document.querySelector(".js-button");
+    let body = document.querySelector(".js-body");
+    let link = document.querySelector(".js-link");
+    let section = document.querySelector(".js-section");
+    let image = document.querySelector(".js-image");
+    let themeName = document.querySelector(".js-themeName");
+    
+    button.addEventListener("click", onChangeBackgroundClick);
+    
+    
+}
